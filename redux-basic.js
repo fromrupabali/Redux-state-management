9665ea53f25922e75007773717cd7b4dd9c5a29c
fromrupabali <mominkeno@gmail.com>
2019-11-28 13:36:52 -0800

    Basic redux added

---
+++ redux-basic.js
@@ -0,0 +1,37 @@
+const redux = require('redux');
+
+const createStore = redux.createStore;
+
+const initialState = {
+    counter : 0
+}
+//Reducer
+const rootReducer = (state = initialState, action) => {
+    if(action.type === 'INC_COUNTER'){
+        return{
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if(action.type === 'ADD_COUNTER'){
+        return{
+            ...state,
+            counter: state.counter + action.payload.value
+        }
+    }
+    return state
+}
+//Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+//Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
+//Dispatching action
+store.dispatch({type:'INC_COUNTER'});
+store.dispatch({type:'ADD_COUNTER',  payload:{value: 5}})
+console.log(store.getState());
+
